d4da01c29d823b6d751bf147a5445595712e0fd9
SandS Arts <sandsarts.developer@gmail.com>
2019-01-22 03:13:57 -0800

    Add files via upload

---
+++ Script_1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_1 : MonoBehaviour
+{
+    public float health;
+    // Start is called before the first frame update
+    void Start()
+    {
+       health-=7f; 
+    } 
+
+    void Print()
+    {
+        Debug.Log("Hello");
+    }
+
+  
+}
---
+++ Script_2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_2 : MonoBehaviour
+{
+    public Script_1 s;
+    // Start is called before the first frame update
+    void Start()
+    {
+       s.Print();
+    }
+    void Update()
+    {
+    
+    
+    }
+    void OnCollisionEnter(Collision o)
+    {
+        Debug.Log(o.gameObject.GetComponent<Script_1>().health);
+        o.gameObject.GetComponent<Script_1>().health=30f;
+        o.gameObject.GetComponent<Script_1>().Print();
+
+    }
+     
+    
+}
